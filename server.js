7dbd9cd4b2b013a127b34dd2565df50700cc4498
Rouslan <rouslanlakhno@gmail.com>
2025-05-17 14:20:55 -0400

    Fix: CORS to support Railway homepage

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: true,
   credentials: true
 
 }));
