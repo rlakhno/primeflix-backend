ab5a4e30561ad40c1146c9f5703a8b3249769319
Rouslan <rouslanlakhno@gmail.com>
2025-05-17 13:51:03 -0400

    Update pool.js to use DATABASE_URL for Railway deployment

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,14 @@
 //pool.js
 require('dotenv').config();
 const { Pool } = require('pg');
+const { connectionString } = require('pg/lib/defaults');
 
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
