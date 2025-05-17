82b97f014868394a733ee2c41d2b693a36db7f08
Rouslan <rouslanlakhno@gmail.com>
2025-05-17 14:41:19 -0400

    Fix: PORT back to 8080 to support Railway homepage

--- server.js
+++ server.js
@@ -268,6 +268,7 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
+console.log('About to start server...');
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
