2322b22dabb9d45809d772986a8fda12c0a32c43
Rouslan <rouslanlakhno@gmail.com>
2025-05-18 17:04:20 -0400

    Fix: Add PORT to support Railway homepage

--- server.js
+++ server.js
@@ -245,8 +245,11 @@ app.post("/checkout", async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel"
+      // success_url: "http://localhost:3000/success",
+      // cancel_url: "http://localhost:3000/cancel"
+      success_url: "https://primeflix-frontend.up.railway.app/success",
+      cancel_url: "https://primeflix-frontend.up.railway.app/cancel"
+
     });
 
 
